ed50fe86bc3567e3204deb96fdef0fb095972c6f
JuliaHsu <hsu.bd100@gmail.com>
2014-04-01 15:54:42 +0800

    加了功能按鍵
    我新增了功能按鍵

--- caculator/src/calculator.java
+++ caculator/src/calculator.java
@@ -12,13 +12,25 @@ public class calculator extends JFrame {
 		jtfResult.setBackground(Color.WHITE);
 		JPanel p1 = new JPanel(new GridLayout(1,1) );
 		p1.add(jtfResult);
-		jbt = new JButton[27];
+		jbt = new JButton[25];
 		
 		JPanel p2 = new JPanel(new GridLayout(5,3) );
 		for (int i = 0; i <= 9; i++){
 			jbt[i] = new JButton(i + "");
 			jbt[i].addActionListener(new ButtonListener());
 		}
+		jbt[12] = new JButton("print");
+		jbt[12].addActionListener(new ButtonListener());
+		jbt[12].setForeground(Color.RED);
+		p2.add(jbt[12]);
+		jbt[13] = new JButton("C");
+		jbt[13].addActionListener(new ButtonListener());
+		jbt[13].setForeground(Color.RED);
+		p2.add(jbt[13]);
+		jbt[14] = new JButton("log");
+		jbt[14].addActionListener(new ButtonListener());
+		p2.add(jbt[14]);
+		jbt[14].setForeground(Color.RED);
 		p2.add(jbt[7]);
 		p2.add(jbt[8]);
 		p2.add(jbt[9]);
@@ -35,8 +47,25 @@ public class calculator extends JFrame {
 		jbt[11] = new JButton("π");
 		jbt[11].addActionListener(new ButtonListener());
 		p2.add(jbt[11]);
+		JPanel p3 = new JPanel(new GridLayout(5,2));
+		jbt[15] = new JButton("sin");
+		jbt[16] = new JButton("cos");
+		jbt[17] = new JButton("tan");
+		jbt[18] = new JButton("^");
+		jbt[19] = new JButton("+");
+		jbt[20] = new JButton("-");
+		jbt[21] = new JButton("×");
+		jbt[22] = new JButton("÷");
+		jbt[23] = new JButton("±");
+		jbt[24] = new JButton("=");
+		for(int i = 15; i <= 24; i++){
+			p3.add(jbt[i]);
+			jbt[i].setForeground(Color.RED);
+			jbt[i].addActionListener(new ButtonListener());
+		}
 		add(p1,BorderLayout.NORTH);
-		add(p2,BorderLayout.SOUTH);
+		add(p2,BorderLayout.CENTER);
+		add(p3,BorderLayout.EAST);
 	}
 	private class ButtonListener implements ActionListener{
 		
