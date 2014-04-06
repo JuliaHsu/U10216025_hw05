b819906c20f9984179cc81a6e4ab69ec50c82072
JuliaHsu <hsu.bd100@gmail.com>
2014-04-06 21:44:07 +0800

    Update calculator.java

--- caculator/src/calculator.java
+++ caculator/src/calculator.java
@@ -1,29 +1,40 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+//Create a class named calculator which extends JFrame
 public class calculator extends JFrame {
-	
+	// New the object of JTextfield, JButton 
 	private JTextField jtfResult = new JTextField();
 	private JButton jbt[];
+	//Initial the solution 
 	private double sol = 0;
 	private String now_num = "";
+	//Initial the temp
 	private double temp = 0;
+	//Initial the option
 	private int option = 0;
 	private boolean reset = false;
+	//Constructor of calculator
 	public calculator(){
+		
 		jtfResult = new JTextField("0");
+		//The text field cannot be edited
 		jtfResult.setEditable(false);
+		//Set the background color
 		jtfResult.setBackground(Color.WHITE);
 		JPanel p1 = new JPanel(new GridLayout(1,1) );
+		//Add the p1 panel on the north side
 		p1.add(jtfResult,BorderLayout.NORTH);
+		//There are 25 buttons
 		jbt = new JButton[25];
-		
+		//Create the p2 panel
 		JPanel p2 = new JPanel(new GridLayout(5,3) );
+		//Create the button of 0~9
 		for (int i = 0; i <= 9; i++){
 			jbt[i] = new JButton(i + "");
 			jbt[i].addActionListener(new ButtonListener());
 		}
+		//Create other operation buttons
 		jbt[12] = new JButton("C");
 		jbt[12].addActionListener(new ButtonListener());
 		jbt[12].setForeground(Color.RED);
@@ -73,13 +84,16 @@ public class calculator extends JFrame {
 		add(p2,BorderLayout.CENTER);
 		add(p3,BorderLayout.EAST);
 	}
+	//Handle the buttons
 	private class ButtonListener implements ActionListener{
 		
 		public void actionPerformed(ActionEvent e){
+			//The button of numbers
 			if(e.getSource() == jbt[0] || e.getSource() == jbt[1] || e.getSource() == jbt[2] ||
 				e.getSource() == jbt[3] || e.getSource() == jbt[4] || e.getSource() == jbt[5]
 				|| e.getSource() == jbt[6] || e.getSource() == jbt[7] || e.getSource() == jbt[8]
 				|| e.getSource() == jbt[9]){
+				//Get the number of the text field
 				now_num = jtfResult.getText();
 				if(reset){
 				reset = false;
@@ -90,14 +104,17 @@ public class calculator extends JFrame {
                     jtfResult.setText(now_num+e.getActionCommand());
 				}
 			}
+			//Set PI
 			else if(e.getSource() == jbt[11]){
 				double now = Math.PI;
 				jtfResult.setText(String.format("%.12f", now));
 			}
+			
 			else if(e.getSource() == jbt[10]){
 				now_num = jtfResult.getText();
 				jtfResult.setText(now_num+ ".");
 			}
+			//Set +
 			else if(e.getSource()==jbt[19]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -113,6 +130,7 @@ public class calculator extends JFrame {
 				}
 				jtfResult.setText(sol +"");
 			}
+			//Set -
 			else if(e.getSource() == jbt[20]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -128,6 +146,7 @@ public class calculator extends JFrame {
 				}
 				jtfResult.setText(sol+"");
 			}
+			//Set *
 			else if (e.getSource() == jbt[21]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -143,6 +162,7 @@ public class calculator extends JFrame {
 				}
 				jtfResult.setText(sol + "");
 			}
+			//Set /
 			else if(e.getSource() == jbt[22]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -157,6 +177,7 @@ public class calculator extends JFrame {
 					reset = true;
 				}
 			}
+			//Set sin
 			else if(e.getSource() == jbt[15]){
 				double now = Double.parseDouble(jtfResult.getText());
 				temp = now;
@@ -164,6 +185,7 @@ public class calculator extends JFrame {
 				sol = operation(option,sol,temp);
 				reset = true;
 			}
+			//Set cos
 			else if(e.getSource() == jbt[16]){
 				double now = Double.parseDouble(jtfResult.getText());
 				temp = now;
@@ -171,6 +193,7 @@ public class calculator extends JFrame {
 				sol = operation(option, sol, temp);
 				reset = true;
 			}
+			//Set tan
 			else if(e.getSource() == jbt[17]){
 				double now = Double.parseDouble(jtfResult.getText());
 				temp = now;
@@ -179,7 +202,7 @@ public class calculator extends JFrame {
 				reset = true;
 			}
 			
-			// =
+			//Set =
 			else if(e.getSource() == jbt[24]){
 				double now = Double.parseDouble(jtfResult.getText());
 				temp = now;
@@ -188,6 +211,7 @@ public class calculator extends JFrame {
 				reset = true;
 				jtfResult.setText(sol+ "");
 			}
+			//Set ^(pow)
 			else if(e.getSource() == jbt[18]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -202,6 +226,7 @@ public class calculator extends JFrame {
 					reset = true;
 				}
 			}
+			//Set C
 			else if(e.getSource() == jbt[12]){
 				temp = 0;
 				sol = 0;
@@ -209,6 +234,7 @@ public class calculator extends JFrame {
 				reset = true;
 				jtfResult.setText("0");
 			}
+			//Set 1/x
 			else if(e.getSource() == jbt[13]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -223,6 +249,7 @@ public class calculator extends JFrame {
 					reset = true;
 				}
 			}
+			//Set log10
 			else if(e.getSource() == jbt[14]){
 				double now = Double.parseDouble(jtfResult.getText());
 				if(option == 0){
@@ -240,7 +267,7 @@ public class calculator extends JFrame {
 			}
 			
 	
-		
+	//The method of operation	
 	public double operation(int option, double sol,double temp){
 		if(option == 1){
 			sol = sol + temp;
@@ -275,7 +302,7 @@ public class calculator extends JFrame {
 		return sol;
 	}
 	}
-
+	//main method
 	public static void main(String[] args){
 		calculator c = new calculator();
 		c.setTitle("Calculator");
